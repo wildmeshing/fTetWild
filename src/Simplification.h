7f8d795688918e70d787272c2cde4c5c480af36b
YIxin-Hu <yh1998@nyu.edu>
2019-12-18 16:49:07 -0500

    add license into files

--- src/Simplification.h
+++ src/Simplification.h
@@ -1,3 +1,11 @@
+// This file is part of fTetWild, a software for generating tetrahedral meshes.
+//
+// Copyright (C) 2019 Yixin Hu <yixin.hu@nyu.edu>
+// This Source Code Form is subject to the terms of the Mozilla Public License
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can
+// obtain one at http://mozilla.org/MPL/2.0/.
+//
+
 #ifndef FLOATTETWILD_SIMPLIFICATION_H
 #define FLOATTETWILD_SIMPLIFICATION_H
 
